c519396701b71e3fdf87c1f274bc2b2bdaa7eca5
Davis <Davis@qq.com>
2025-12-18 11:28:13 +0800

    feat: init

---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig, loadEnv, type ConfigEnv, type UserConfig  } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vite.dev/config/
+export default defineConfig(({ mode } : ConfigEnv): UserConfig => {
+  const env = loadEnv(mode, process.cwd())
+  return {
+    plugins: [vue()],
+    server: {
+      port: 5173,
+      host: env.VITE_HOST || 'localhost',
+      proxy: {
+        '/api': {
+          target: env.VITE_API_BASE_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        }
+      }
+    },
+    build: {
+      sourcemap: env.VITE_SOURCEMAP === 'true',
+      minify: mode === 'production' ? 'terser' : false,
+    },
+    define: {
+      __WEB_VERSION__: JSON.stringify(env.VITE_APP_ENV),
+    }
+  }
+})
